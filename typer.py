491a9665a6d5aa8d7ffc3515945e62d7ac6fc096
ABHIJEtsingh <94282804+ABHIJEtsingh@users.noreply.github.com>
2025-03-19 10:41:20 +0530

    Update typer.py
    Used Pyautogui for image detection and json for data loading

--- typer.py
+++ typer.py
@@ -1,42 +1,74 @@
-from pynput.keyboard import Key, Controller
-import time
+import json
+import pyautogui
+import time  # Import the time module
 
-keyboard = Controller()
+import json
+import pyautogui
 
-def split(words):
-    return [char for char in words]
+# Load the data from the JSON file
+with open('105th.json') as f:
+    data_file = json.load(f)
+    
 
-time.sleep(3)
+# Define the order in which you want to input the data, excluding "Height" and "Weight"
+input_order = [
+    "Name",
+    "Gender",
+    "Age",
+    "Marital Status",
+    "Education",
+    "Detail",
+    "Occupation",
+    "Religion",
+    "Caste",
+    "Sub Caste",
+    "Gothram",
+    "Mother Tongue",
+    "Horoscope Match",
+    "Star",
+    "Rassi/Moon Sign",
+    "Dhosham / Magalik",
+    "Height",
+    "Weight",
+    "Height",
+    "Weight",
+    "Height",
+    "Citizen of",
+    "Home State",
+    "Home State",
+    "Country Living",
+    "Home State",
+    "Name",
+    "Name",
+    "Body Type",
+    "Complexion",
+    "Physical Status",
+    "Eating Habit",
+    "Smoke Habit",
+    "Drink Habit",
+    "Family Value",
+    "Family",
+    "Family Status",
+    "Annual Income",
+    "About Family",
+    "More Description",
+    "Expectations"
+]
+additional_text = {
+    "password": "1234",
+    "How to know about us": "friends",
+}
 
-# First input with a time interval of 2 seconds
-for i in split("""One elder brother married and one younger sister married.Family setteled in Delhi.Father and mother
-are now enjoying their retire life.
-"""):
-    keyboard.type(i)
-    time.sleep(0.1)
-time.sleep(2)  # Time interval between the first and second inputs
+# Find the starting position for typing
+x, y = pyautogui.locateCenterOnScreen("field.png", confidence=0.8)
+pyautogui.moveTo(x, y, duration=1)
+pyautogui.leftClick()
 
-# Second input with a time interval of 1 second
-for i in split("""YOUR TEXT """):
-    keyboard.type(i)
-    time.sleep(0.1)
-
-time.sleep(2)
-for i in split("""YOUR SECOND TEXT"""):
-    keyboard.type(i)
-    time.sleep(0.1)
-
-time.sleep(2)
-for i in split("1234"):
-    keyboard.type(i)
-    time.sleep(0.1)
-
-time.sleep(2)
-for i in split("1234"):
-    keyboard.type(i)
-    time.sleep(0.1)
-
-time.sleep(2)
-for i in split("Friend"):
-    keyboard.type(i)
-    time.sleep(0.1)
+# Loop through the data in the specified order and type it
+for field in input_order:
+    if field not in [ "Height", "Weight"]:
+        value = data_file.get(field, "")  # Get the value from the JSON data, default to an empty string
+        if field in additional_text:
+            value += additional_text[field]
+        pyautogui.typewrite(value)
+    pyautogui.press("tab")
